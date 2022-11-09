b24e08f0ff93744055de44476628911011c83995
MonikaPandian <panmonikmm@gmail.com>
2022-11-09 16:15:30 +0530

    url-shortener

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import "./signup.css"
 import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const ResetPassword = () => {
     const navigate = useNavigate();
+    const {id, token} = useParams();
+    const [newPassword, setNewPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const updatePassword = {
+           password: newPassword                     
+        }     
+
+        fetch(`http://localhost:9005/users/reset-password/${id}/${token}`, {
+            method: "POST",
+            body: JSON.stringify(updatePassword),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+            .then((data) => data.json())
+            .then((res) => { console.log(res)
+                if(res.message === "user not exists!!!"){
+                    window.alert("user not exists!!!. If you are a new user sign up to create an account")
+                }
+                else if(res.message === "Account is not activated"){
+                    window.alert("Account is not activated. Please login to activate your account.")
+                }
+                else if(res.message === "success"){
+                    window.alert("Password reset link sent successfully to your mail")                                     
+                }
+                else{
+                    window.alert("Internal server error. please try again later")
+                }
+            })
+            .catch((e) => console.log(e));
+    }
+
     return (
         <div className="container-scroller">
             <div className="container-fluid page-body-wrapper">
@@ -30,15 +66,15 @@ const ResetPassword = () => {
                                 <div className="brand-logo">
                                     <h3>Reset Password</h3>
                                 </div>
-                                <form className="pt-3">
+                                <form onSubmit={handleSubmit} className="pt-3">
                                     <div className="form-group">
-                                        <input type="password" className="form-control form-control-lg" id="exampleInputEmail" placeholder="New Password" />
+                                        <input type="password" value={newPassword} onChange={(e)=>setNewPassword(e.target.value)} className="form-control form-control-lg" id="exampleInputEmail" placeholder="New Password" />
                                     </div>
                                     <div className="form-group">
-                                        <input type="password" className="form-control form-control-lg" id="exampleInputEmail" placeholder="Confirm Password" />
+                                        <input type="password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} className="form-control form-control-lg" id="exampleInputEmail" placeholder="Confirm Password" />
                                     </div>
                                     <div className="mt-3">
-                                        <span className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">UPDATE PASSWORD</span>
+                                        <button type="submit" className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">UPDATE PASSWORD</button>
                                     </div>
                                 </form>
                             </div>
